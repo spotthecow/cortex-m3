cee720dc799d3804da45a75418d3377711554d4a
spot <spot@spotthecow.com>
2025-02-26 23:38:50 -0800

    configure for running on target

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,14 @@ use core::panic::PanicInfo;
 
 #[no_mangle]
 pub unsafe extern "C" fn Reset() -> ! {
+    let _x = 66;
+
     loop {}
 }
+
+#[link_section = ".vector_table.reset_vector"]
+#[no_mangle]
+pub static RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
 
 #[panic_handler]
 fn panic(_: &PanicInfo<'_>) -> ! {
